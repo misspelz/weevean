8518b37bafbe4c29f21ad67526b06bae2600d574
emmaccen <oriolaemmanuel199@gmail.com>
2026-01-25 23:41:43 +0100

    UI (Components), Utils: add components and utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getUserInitials = (name: string | null | undefined) => {
+  if (!name) return "";
+  const namesArray = name.trim().split(" ");
+  if (namesArray.length === 0) return "";
+
+  const initials =
+    namesArray.length === 1
+      ? namesArray[0].charAt(0)
+      : namesArray[0].charAt(0) + namesArray[namesArray.length - 1].charAt(0);
+
+  return initials.toUpperCase();
+};
