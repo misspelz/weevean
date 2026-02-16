204bda69cbc99ea8393f49e56175f24b41414f9b
emmaccen <oriolaemmanuel199@gmail.com>
2026-02-16 18:32:50 +0100

    UI: Add headers

---
+++ components/shared/app-header-partial.tsx
@@ -0,0 +1,20 @@
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import * as React from "react";
+const AppHeaderPartial = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+      <div className="flex items-center gap-2 px-4 w-full">
+        <SidebarTrigger className="-ml-1" />
+        <Separator orientation="vertical" className="mr-2 h-4" />
+        {children}
+      </div>
+    </header>
+  );
+};
+
+export default AppHeaderPartial;
