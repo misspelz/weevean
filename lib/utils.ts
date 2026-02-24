9abd5c701d39ab4db4088b07b7ee6585875f34cc
emmaccen <oriolaemmanuel199@gmail.com>
2026-02-24 01:56:35 +0100

    Util: Add fetch handler

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { ClientError } from "./client-error";
+import { ApiResponse } from "./types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -16,4 +18,22 @@ export const getUserInitials = (name: string | null | undefined) => {
       : namesArray[0].charAt(0) + namesArray[namesArray.length - 1].charAt(0);
 
   return initials.toUpperCase();
+};
+
+export const fetcher = async <T>(
+  input: string | URL | Request,
+  init?: RequestInit | undefined,
+  onErrorMessage?: string,
+): Promise<T> => {
+  const response = await fetch(input, init);
+  if (!response.ok) {
+    if (onErrorMessage) {
+      const clientError = new ClientError(response);
+      clientError.handleAllCommonErrorsWithToast(onErrorMessage);
+    }
+    throw await response.json();
+  }
+  const json: ApiResponse<T> = await response.json();
+
+  return json.result ? json.result.data : (json as unknown as T);
 };
